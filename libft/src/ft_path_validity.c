bbf5f8554201e49cb904b8b335a3b426f9d9abfa
Juha Manner <jumanner@student.hive.fi>
2022-04-01 14:33:09 +0300

    Added ft_path_is_within_limits() to libft.

--- libft/src/ft_path_validity.c
+++ libft/src/ft_path_validity.c
@@ -6,7 +6,7 @@
 /*   By: jumanner <jumanner@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/18 15:21:28 by jumanner          #+#    #+#             */
-/*   Updated: 2022/03/30 13:36:12 by jumanner         ###   ########.fr       */
+/*   Updated: 2022/04/01 14:29:55 by jumanner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,4 +35,32 @@ int	ft_path_has_valid_link(const char *path)
 	if (stat(path, &temp) != -1)
 		return (1);
 	return (0);
+}
+
+/*
+ * Returns true if the given path as a whole does not exceed PATH_MAX, and
+ * each individual name on the path does not exceed NAME_MAX.
+ */
+int	ft_path_is_within_limits(const char *path)
+{
+	char	**names;
+	size_t	i;
+
+	if (ft_strlen(path) >= PATH_MAX)
+		return (0);
+	names = ft_strsplit(path, '/');
+	if (!names)
+		return (0);
+	i = 0;
+	while (names[i])
+	{
+		if (ft_strlen(names[i]) >= NAME_MAX)
+		{
+			ft_free_null_array((void **)names);
+			return (0);
+		}
+		i++;
+	}
+	ft_free_null_array((void **)names);
+	return (1);
 }
