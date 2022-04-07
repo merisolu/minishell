ffa4dd40d2b41e836af0ecffeb5eace34cf43c55
Juha Manner <jumanner@student.hive.fi>
2022-04-07 13:08:32 +0300

    Built-in setenv now takes env_set function return value into account.

--- src/set_env.c
+++ src/set_env.c
@@ -6,7 +6,7 @@
 /*   By: jumanner <jumanner@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/01 15:04:07 by jumanner          #+#    #+#             */
-/*   Updated: 2022/04/06 13:12:24 by jumanner         ###   ########.fr       */
+/*   Updated: 2022/04/07 13:06:39 by jumanner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,5 @@ int	set_env(char *const *args, char *const **env)
 	if (!(ft_isalpha(name[0]) || name[0] == '_'))
 		return (print_error("Variable name must begin with a letter.", 1));
 	value = args[2];
-	env_set(name, value, env);
-	return (0);
+	return (!env_set(name, value, env));
 }
