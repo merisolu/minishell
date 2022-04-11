a1b01632526b9992b51fbfaf4a4ca4fe62e3aecb
Juha Manner <jumanner@student.hive.fi>
2022-04-11 09:27:52 +0300

    Updated comments on env array duplication, and renamed env_copy to var_cpy.

--- src/main.c
+++ src/main.c
@@ -6,14 +6,13 @@
 /*   By: jumanner <jumanner@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/25 13:13:35 by jumanner          #+#    #+#             */
-/*   Updated: 2022/04/07 09:10:05 by jumanner         ###   ########.fr       */
+/*   Updated: 2022/04/11 09:17:34 by jumanner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-// TODO: Error on failed malloc.
-static void	*env_copy(void *var)
+static void	*var_cpy(void *var)
 {
 	return ((void *)ft_strdup((const char *)var));
 }
@@ -23,7 +22,8 @@ static t_state	get_state_struct(char *const **env)
 	t_state	result;
 
 	ft_bzero(&result, sizeof(t_state));
-	ft_dup_null_array((void **)*env, (void ***)&(result.env), env_copy);
+	// TODO: Check dup array result.
+	ft_dup_null_array((void **)*env, (void ***)&(result.env), var_cpy);
 	return (result);
 }
 
