22ba789a7b01a5c9dbdb3eaa9349d62c406c5800
Juha Manner <jumanner@student.hive.fi>
2022-04-14 14:49:50 +0300

    Added env duplication error check.

--- src/main.c
+++ src/main.c
@@ -6,20 +6,16 @@
 /*   By: jumanner <jumanner@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/25 13:13:35 by jumanner          #+#    #+#             */
-/*   Updated: 2022/04/14 13:25:55 by jumanner         ###   ########.fr       */
+/*   Updated: 2022/04/14 14:48:13 by jumanner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static t_state	get_state_struct(char *const **env)
+static int	get_state_struct(char *const **env, t_state *res)
 {
-	t_state	result;
-
-	ft_bzero(&result, sizeof(t_state));
-	// TODO: Check dup array result.
-	ft_dup_null_array((void **)*env, (void ***)&(result.env), var_cpy);
-	return (result);
+	ft_bzero(res, sizeof(t_state));
+	return (ft_dup_null_array((void **)*env, (void ***)&(res->env), var_cpy));
 }
 
 int	main(const int argc, const char **argv, char *const *env)
@@ -29,9 +25,9 @@ int	main(const int argc, const char **argv, char *const *env)
 	char		**args;
 	int			line_read_result;
 
-	(void)argc;
-	(void)argv;
-	state = get_state_struct(&env);
+	(void)argc, argv;
+	if (!get_state_struct(&env, &state))
+		exit(print_error(ERR_MALLOC_FAIL, 1));
 	while (1)
 	{
 		ft_putstr(PROMPT);
