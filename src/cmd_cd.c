d4272ace583fb6f89d81697acaa349a4d292f8a4
Juha Manner <jumanner@student.hive.fi>
2022-04-19 15:35:10 +0300

    Added path normalization to built-in cd command.

--- src/cmd_cd.c
+++ src/cmd_cd.c
@@ -6,7 +6,7 @@
 /*   By: jumanner <jumanner@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/31 15:27:43 by jumanner          #+#    #+#             */
-/*   Updated: 2022/04/19 10:50:19 by jumanner         ###   ########.fr       */
+/*   Updated: 2022/04/19 15:33:30 by jumanner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,6 +40,7 @@ static int	construct_path(char *target, char **result)
 		if (!path)
 			return (print_error(ERR_CANNOT_GET_CWD, 0));
 		ft_path_join(path, target, result);
+		free(path);
 	}
 	return (1);
 }
@@ -49,15 +50,12 @@ int	cmd_cd(char *const *args, char *const **env)
 	char	*target;
 	char	*path;
 	size_t	arg_count;
+	int		return_value;
 
 	arg_count = ft_null_array_len((void **)args);
 	if (arg_count > 2)
 		return (print_error(ERR_TOO_MANY_ARGS, 1));
-	if (arg_count == 1)
-		path = env_get("HOME", *env);
 	target = get_target(args, arg_count, *env);
-	if (!target)
-		return (0);
 	if (!ft_path_is_within_limits(target))
 		return (print_error(ERR_INVALID_PATH, 1));
 	if (!construct_path(target, &path))
@@ -68,6 +66,10 @@ int	cmd_cd(char *const *args, char *const **env)
 		return (print_error(ERR_INVALID_PATH, 1));
 	if (env_get("PWD", *env))
 		env_set("OLDPWD", env_get("PWD", *env), env);
-	env_set("PWD", path, env);
-	return (ft_abs(chdir(path)));
+	ft_normalize_path(path, &target);
+	env_set("PWD", target, env);
+	return_value = chdir(target);
+	free(target);
+	free(path);
+	return (return_value);
 }
