e559fa85a358a655a6b28571fe6c8b773ee5d04a
Juha Manner <jumanner@student.hive.fi>
2022-04-22 14:11:50 +0300

    Added ft_strnjoin() to libft.

--- libft/include/libft.h
+++ libft/include/libft.h
@@ -6,7 +6,7 @@
 /*   By: jumanner <jumanner@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/28 16:24:04 by jumanner          #+#    #+#             */
-/*   Updated: 2022/04/19 15:18:13 by jumanner         ###   ########.fr       */
+/*   Updated: 2022/04/22 13:23:31 by jumanner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -122,6 +122,7 @@ char		*ft_strmap(char const *s, char (*f)(char));
 char		*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 char		*ft_strsub(char const *s, unsigned int start, size_t len);
 char		*ft_strjoin(char const *s1, char const *s2);
+char		*ft_strnjoin(char const *s1, char const *s2, size_t n);
 char		*ft_strtrim(char const *s);
 char		**ft_strsplit(char const *s, char c);
 char		*ft_strtolower(char *s);
--- libft/src/ft_strjoin.c
+++ libft/src/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: jumanner <jumanner@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 09:48:18 by jumanner          #+#    #+#             */
-/*   Updated: 2021/11/25 14:10:45 by jumanner         ###   ########.fr       */
+/*   Updated: 2022/04/22 14:10:52 by jumanner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,37 @@ char	*ft_strjoin(char const *s1, char const *s2)
 		return (NULL);
 	s1_len = ft_strlen(s1);
 	s2_len = ft_strlen(s2);
+	result = ft_strnew(s1_len + s2_len);
+	if (!result)
+		return (NULL);
+	i = 0;
+	while (i < s1_len)
+	{
+		result[i] = s1[i];
+		i++;
+	}
+	while (i - s1_len < s2_len)
+	{
+		result[i] = s2[i - s1_len];
+		i++;
+	}
+	result[s1_len + s2_len] = '\0';
+	return (result);
+}
+
+/*
+ * Returns a new string, which is the result of combining s1 and n chars
+ * from s2. Returns NULL on error.
+ */
+char	*ft_strnjoin(char const *s1, char const *s2, size_t n)
+{
+	char	*result;
+	size_t	s1_len;
+	size_t	s2_len;
+	size_t	i;
+
+	s1_len = ft_strlen(s1);
+	s2_len = ft_min_size_t(n, ft_strlen(s2));
 	result = ft_strnew(s1_len + s2_len);
 	if (!result)
 		return (NULL);
