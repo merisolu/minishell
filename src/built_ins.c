bbb2feb029953fadf9551079d66f8c1f0b27aca6
Juha Manner <jumanner@student.hive.fi>
2022-05-10 15:20:31 +0300

    Return value of built-in commands is now stored.

--- src/built_ins.c
+++ src/built_ins.c
@@ -6,7 +6,7 @@
 /*   By: jumanner <jumanner@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/31 12:18:43 by jumanner          #+#    #+#             */
-/*   Updated: 2022/05/10 11:44:03 by jumanner         ###   ########.fr       */
+/*   Updated: 2022/05/10 15:16:41 by jumanner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,5 +37,9 @@ t_cmd	*get_built_in(const char *name)
 
 int	run_built_in(t_cmd cmd, char *const *args, t_state *state)
 {
-	return (cmd(args, state));
+	int	return_value;
+
+	return_value = cmd(args, state);
+	set_return_value(return_value, state);
+	return (return_value);
 }
