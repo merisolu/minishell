bed7e61906a6622f61f5601bb9beebd9d4338b16
Juha Manner <jumanner@student.hive.fi>
2022-06-13 17:09:14 +0300

    Continued work on autocomplete.

--- src/autocomplete.c
+++ src/autocomplete.c
@@ -6,66 +6,81 @@
 /*   By: jumanner <jumanner@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 10:07:51 by jumanner          #+#    #+#             */
-/*   Updated: 2022/06/13 11:35:01 by jumanner         ###   ########.fr       */
+/*   Updated: 2022/06/13 17:07:01 by jumanner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-// TODO: This fails when the string begins with whitespace
-char	*find_from_path(char *path, char *partial_name)
+// TODO: Check that file can be executed?
+/*
+ * Searches the specific path for an executable file with a name matching
+ * partial_name. The resulting file name will be put in *result. *result will
+ * need to be freed once it's not used. It will be NULL if no match is found,
+ * or on error.
+ */
+static int	search_path(char *path, char *partial_name, char **result)
 {
-	char			*result;
 	DIR				*dir;
 	struct dirent	*entry;
 
+	*result = NULL;
 	dir = opendir(path);
 	if (!dir)
-		return (NULL);
+		return (0);
 	entry = readdir(dir);
 	while (entry)
 	{
 		if (ft_strnequ(partial_name, entry->d_name, ft_strlen(partial_name)))
 		{
-			result = ft_strdup(entry->d_name); // TODO: Malloc check?
+			*result = ft_strdup(entry->d_name);
 			closedir(dir);
-			return (result);
+			if (!result)
+				return (-1);
+			return (1);
 		}
 		entry = readdir(dir);
 	}
 	closedir(dir);
-	return (NULL);
+	return (0);
 }
 
-char	*find_from_paths(t_state *state)
+/*
+ * Searches for a binary that at least partially matches state->input and is
+ * located in any of the folders listed in the PATH environment variable.
+ *
+ * *result will be NULL if no match was found, or if an error has occured.
+ */
+static void	search_from_paths(t_state *state, char **result)
 {
-	char	*result;
+	int		search_return_value;
+	char	*trimmed_input;
 	char	**paths;
 	size_t	i;
 
-	if (!env_get("PATH", state->env))
-		return (NULL);
+	trimmed_input = ft_strtrim(state->input);
 	paths = ft_strsplit(env_get("PATH", state->env), ':');
-	if (!paths)
+	if (!trimmed_input || !paths)
 	{
-		print_error(ERR_MALLOC_FAIL, 0);
-		return (NULL);
+		print_error(ERR_MALLOC_FAIL, -1);
+		free(trimmed_input);
+		ft_free_null_array((void **)paths);
+		return ;
 	}
 	i = 0;
 	while (paths[i])
 	{
-		result = find_from_path(paths[i], state->input);
-		if (result)
-		{
-			ft_free_null_array((void **)paths);
-			return (result);
-		}
+		search_return_value = search_path(paths[i], trimmed_input, result);
+		if (search_return_value == -1)
+			print_error(ERR_MALLOC_FAIL, -1);
+		if (search_return_value != 0)
+			break ;
 		i++;
 	}
-	return (ft_free_null_array((void **)paths));
+	free(trimmed_input);
+	ft_free_null_array((void **)paths);
 }
 
-// TODO: Return errors.
 /*
  * Attempts to autocomplete a partical binary name in state->input. If a match
  * is found, the partial name will be replaced with the full name.
@@ -75,8 +90,9 @@ void	autocomplete(t_state *state)
 	char	*temp;
 
 	if (ft_strlen(state->input) == 0)
-		return;
-	temp = find_from_paths(state);
+		return ;
+	temp = NULL;
+	search_from_paths(state, &temp);
 	if (temp)
 	{
 		free(state->input);
@@ -84,5 +100,5 @@ void	autocomplete(t_state *state)
 		state->cursor = ft_strlen(PROMPT) + ft_strlen(state->input);
 	}
 	print_state(state, 0);
-	return;
+	return ;
 }
