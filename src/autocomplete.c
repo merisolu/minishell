52c8208eeec3c4a924807d5221e88f131b39ba16
Juha Manner <jumanner@student.hive.fi>
2022-06-28 15:40:33 +0300

    Fixed autocomplete not checking if the trimmed input was empty.

--- src/autocomplete.c
+++ src/autocomplete.c
@@ -6,7 +6,7 @@
 /*   By: jumanner <jumanner@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 10:07:51 by jumanner          #+#    #+#             */
-/*   Updated: 2022/06/17 11:01:51 by jumanner         ###   ########.fr       */
+/*   Updated: 2022/06/28 15:37:45 by jumanner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,24 +59,21 @@ static int	search_path(char *path, char *partial_name, char **result)
 }
 
 /*
- * Searches for a binary that at least partially matches state->input and is
+ * Searches for a binary that at least partially matches the given input and is
  * located in any of the folders listed in the PATH environment variable.
  *
  * *result will be NULL if no match was found, or if an error has occured.
  */
-static void	search_from_paths(t_state *state, char **result)
+static void	search_from_paths(char *const *env, char *input, char **result)
 {
 	int		search_return_value;
-	char	*trimmed_input;
 	char	**paths;
 	size_t	i;
 
-	trimmed_input = ft_strtrim(state->input);
-	paths = ft_strsplit(env_get("PATH", state->env), ':');
-	if (!trimmed_input || !paths)
+	paths = ft_strsplit(env_get("PATH", env), ':');
+	if (!paths)
 	{
 		print_error(ERR_MALLOC_FAIL, -1);
-		free(trimmed_input);
 		ft_free_null_array((void **)paths);
 		return ;
 	}
@@ -84,12 +81,11 @@ static void	search_from_paths(t_state *state, char **result)
 	search_return_value = 0;
 	while (paths[i] && search_return_value == 0)
 	{
-		search_return_value = search_path(paths[i], trimmed_input, result);
+		search_return_value = search_path(paths[i], input, result);
 		if (search_return_value == -1)
 			print_error(ERR_MALLOC_FAIL, -1);
 		i++;
 	}
-	free(trimmed_input);
 	ft_free_null_array((void **)paths);
 }
 
@@ -99,18 +95,27 @@ static void	search_from_paths(t_state *state, char **result)
  */
 void	autocomplete(t_state *state)
 {
+	char	*trimmed_input;
 	char	*temp;
 
 	if (ft_strlen(state->input) == 0)
 		return ;
-	temp = search_for_built_in(state->input);
+	trimmed_input = ft_strtrim(state->input);
+	if (ft_strlen(trimmed_input) == 0)
+	{
+		if (!trimmed_input)
+			print_error(ERR_MALLOC_FAIL, 0);
+		free(trimmed_input);
+		return ;
+	}
+	temp = search_for_built_in(trimmed_input);
 	if (!temp)
-		search_from_paths(state, &temp);
+		search_from_paths(state->env, trimmed_input, &temp);
 	if (temp)
 	{
 		free(state->input);
 		state->input = temp;
 		state->cursor = ft_strlen(PROMPT) + ft_strlen(state->input);
 	}
-	return ;
+	free(trimmed_input);
 }
