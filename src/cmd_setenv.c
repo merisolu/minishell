9e89a8ba9fdfb9e1e77e16b61927a943d37288e2
Juha Manner <jumanner@student.hive.fi>
2022-06-29 11:22:18 +0300

    Fixed segfault in setenv.

--- src/cmd_setenv.c
+++ src/cmd_setenv.c
@@ -6,7 +6,7 @@
 /*   By: jumanner <jumanner@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/01 15:04:07 by jumanner          #+#    #+#             */
-/*   Updated: 2022/06/17 11:16:07 by jumanner         ###   ########.fr       */
+/*   Updated: 2022/06/29 11:19:50 by jumanner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,15 +24,17 @@ int	cmd_setenv(char *const *args, t_state *state)
 		return (0);
 	}
 	if (!(ft_isalpha(name[0]) || name[0] == '_'))
-		return (
-			print_named_error(
+		return (print_named_error(
 				"setenv", "Variable name must begin with a letter.", 1
-			)
-		);
+			));
 	value = args[2];
 	if (!value)
 	{
-		value = ft_strchr(name, '=') + 1;
+		value = ft_strchr(name, '=');
+		if (!value)
+			return (0);
+		else
+			value += 1;
 		name[ft_dstchr(name, '=', ft_strlen(name))] = '\0';
 	}
 	return (!env_set(name, value, &(state->env)));
