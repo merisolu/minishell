201f8975aa19485539cd6552f51e183ffe126081
Juha Manner <jumanner@student.hive.fi>
2022-07-27 14:01:10 +0300

    Exit built-in now prints 'exit' when exiting.

--- src/cmd_exit.c
+++ src/cmd_exit.c
@@ -6,7 +6,7 @@
 /*   By: jumanner <jumanner@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/31 16:37:52 by jumanner          #+#    #+#             */
-/*   Updated: 2022/06/16 13:58:10 by jumanner         ###   ########.fr       */
+/*   Updated: 2022/07/27 13:56:15 by jumanner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,6 @@ int	cmd_exit(char *const *args, t_state *state)
 	if (args[1])
 		state->exit_return_value = ft_atoi(args[1]);
 	state->exiting = 1;
+	ft_putendl("exit");
 	return (0);
 }
