a6f9cdc1988c79c7e7ec854c488df02b2158b623
Juha Manner <jumanner@student.hive.fi>
2022-09-02 10:19:52 +0300

    Output is now redrawn on window resize.

--- src/signal.c
+++ src/signal.c
@@ -6,7 +6,7 @@
 /*   By: jumanner <jumanner@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/28 12:20:30 by jumanner          #+#    #+#             */
-/*   Updated: 2022/07/27 11:40:16 by jumanner         ###   ########.fr       */
+/*   Updated: 2022/09/02 10:15:10 by jumanner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@ static void	set_signal_int(int signal)
 void	set_signal_handling(void)
 {
 	signal(SIGINT, set_signal_int);
+	signal(SIGWINCH, set_signal_int);
 }
 
 void	check_signal(t_state *state)
@@ -32,5 +33,7 @@ void	check_signal(t_state *state)
 		save_cursor();
 		print_state(state, 0);
 	}
+	else if (g_last_signal == SIGWINCH)
+		print_state(state, 1);
 	g_last_signal = 0;
 }
